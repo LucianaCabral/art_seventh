db6e79c20f8aab465b2651450253cdb5b8f0cbe7
LucianaCabral <eng.luciana.cabral@gmail.com>
2023-06-29 14:50:47 -0300

    Added:[ASA/15-1] Cria gerenciamento das dependências compartilhadas

---
+++ buildSrc/src/main/java/Modules.kt
@@ -0,0 +1,8 @@
+object Modules {
+    // core
+    const val remote = ":core:data:remote"
+    const val navigation = ":core:common:navigation"
+
+    // features
+    const val trends = ":features:trends"
+}
