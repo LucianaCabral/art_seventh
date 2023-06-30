b7a7561cfe1ec8f61e27387f1d4b22e4bb8fd051
LucianaCabral <eng.luciana.cabral@gmail.com>
2023-06-30 18:31:36 -0300

    Added:[ASA/8-5] Cria a camada de apresentação

---
+++ features/popular/src/main/java/com/lcabral/features/popular/presentation/PopularFragment.kt
@@ -0,0 +1,80 @@
+package com.lcabral.features.popular.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.lcabral.features.popular.R
+import com.lcabral.features.popular.databinding.FragmentPopularBinding
+import com.lcabral.features.popular.presentation.adapter.PopularAdapter
+import com.lcabral.features.popular.presentation.viewmodel.PopularViewModel
+import com.lcabral.features.popular.presentation.viewmodel.PopularViewState
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+internal class PopularFragment : Fragment(R.layout.fragment_popular) {
+
+    private var _binding: FragmentPopularBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: PopularViewModel by viewModel()
+    private val popularAdapter by lazy { PopularAdapter() }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPopularBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.getPopular()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+        setupObservers()
+
+    }
+
+    private fun setupObservers() {
+      handlePopular()
+    }
+
+    private fun handlePopular() {
+       viewModel.viewState.observe(this) { state ->
+           state?.let {
+               if (state.getPopularResultItems?.isNotEmpty() == true)
+                updateList(it)
+           }
+       }
+    }
+
+    private fun updateList(state:PopularViewState) {
+        state.getPopularResultItems?.let { popularAdapter.updateAdapter(it) }
+    }
+
+    private fun setupRecyclerView() {
+        binding.recyclerPopular.apply {
+            setHasFixedSize(true)
+            adapter = popularAdapter
+            layoutManager =
+                LinearLayoutManager(context, GridLayoutManager.VERTICAL, false)
+        }
+    }
+
+
+    companion object {
+        fun newInstance(): PopularFragment = PopularFragment()
+    }
+}
