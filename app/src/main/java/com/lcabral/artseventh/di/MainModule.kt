9005c215cf0b3d62f27df78add32a588b71e9be2
LucianaCabral <eng.luciana.cabral@gmail.com>
2023-07-02 22:34:26 -0300

    Added:[ASA/8-5] Ajusta a navegação para a home

--- app/src/main/java/com/lcabral/artseventh/di/MainModule.kt
+++ app/src/main/java/com/lcabral/artseventh/di/MainModule.kt
@@ -1,13 +1,17 @@
 package com.lcabral.artseventh.di
 
+import com.lcabral.artseventh.navigation.HomeNavigationImpl
+import com.lcabral.core.common.navigation.HomeNavigation
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 object MainModule {
-    val modules get() = listOf(presentation, domainModule, dataModule)
+    val modules get() = listOf(presentation, domainModule, dataModule, additionalModules)
 
     private val dataModule: Module = module { }
     private val domainModule: Module = module { }
     private val presentation: Module = module { }
-
+    private val additionalModules:  Module = module {
+        factory<HomeNavigation> { HomeNavigationImpl() }
+    }
 }
